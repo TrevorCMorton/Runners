cb133ced2181960cccd365c98963d0076d4380ac
Trevor Morton <mortontc@rose-hulman.edu>
2018-11-26 21:58:44 -0500

    Fixed stupid bash

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for (( ; ; ))
 do
-    docker run -it -p 1612:1612 -p 1613:1613 -p 1614:1614 -p 1615:1615 -p 1616:1616 -e prob=$PROB mas_image
+    docker run -it -p 1612:1612 -p 1613:1613 -p 1614:1614 -p 1615:1615 -p 1616:1616 -e PROB=$1 mas_image
 done
