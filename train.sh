5b6074f65303fc571025e5e7b813c0ba285bb63d
Trevor Morton <mortontc@rose-hulman.edu>
2018-12-14 14:49:02 -0500

    Changed training script to only stop one image

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@
 nvidia-docker build -t mas_image$2 .
 for (( ; ; ))
 do
-    nvidia-docker stop $(docker ps -aq)
+    nvidia-docker stop mas_image$2
     nvidia-docker run --memory="4g" --cpus="8" -e DEVICE=$1 -it mas_image$2
 done
