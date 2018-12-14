17145d83feed71cae59203d57aa134e520673b91
Trevor Morton <mortontc@rose-hulman.edu>
2018-12-14 13:31:55 -0500

    Added resource limits to images

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ docker build -t mas_image$2 .
 for (( ; ; ))
 do
     docker stop $(docker ps -aq)
-    docker run -e $1 -it mas_image$2
+    docker run --memory="4g" --cpus="8" -e $1 -it mas_image$2
 done
