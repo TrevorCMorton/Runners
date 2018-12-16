944d37edbf462be405abf831bfb6722f1bd2fe70
Trevor Morton <mortontc@rose-hulman.edu>
2018-12-16 15:57:05 -0500

    Added ability to test locally, added system prune to run script

--- train.sh
+++ train.sh
@@ -3,6 +3,6 @@ nvidia-docker build -t mas_image$2 .
 for (( ; ; ))
 do
     nvidia-docker stop mas_image$2
-    nvidia-docker run --memory="4g" --cpus="8" -e DEVICE=$1 -it mas_image$2
     nvidia-docker system prune
+    nvidia-docker run --memory="4g" --cpus="8" -e DEVICE=$1 -it mas_image$2
 done
